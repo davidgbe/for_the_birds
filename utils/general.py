657e2a65d3f37ab6cd8164a0f0eeec0ae5754547
David Bell <davidgrebell@gmail.com>
2020-08-23 17:17:00 -0700

    add complete dropout experiment

--- utils/general.py
+++ utils/general.py
@@ -36,3 +36,16 @@ def outer_product_n_dim(*args):
     params = [p for p in args]
     outer_product = np.meshgrid(*params, sparse=False, indexing='ij')
     return np.stack([p_vals.flatten() for p_vals in outer_product], axis=1)
+
+# returns a subset of dataframe
+def select(df, selection):
+    criteria = []
+    for col in selection:
+        criteria.append(df[col] == selection[col])
+    return df[np.all(criteria, axis=0)]
+
+# returns a list of unique values in the given Pandas dataframe for each column name specified 
+def to_unique_vals(df, col_names):
+    if type(col_names) is str:
+        col_names = [col_names]
+    return tuple([df[col_name].unique() for col_name in col_names])
